0915089802984156dbbc7da1e85b64a152b0d4ed
mwiko <mwikosinyangwe@gmail.com>
2025-05-12 14:27:28 +0200

    second commit

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from fastapi import FastAPI
 app = FastAPI(title="Hello World API")
 
 @app.get("/greet")
-def greet_user():
+def greet_user(name: str | None = None):
+    if name:
+        return {"message": f"Hello, {name}!"}
     return {"message": "Hello, World!"}
